51cd6a0ffc47c97a8cbc302113e9211af4a5bc20
Anna <bielechanna@gmail.com>
2020-07-24 08:20:18 +0200

    Added change#11

--- NEWprojectJS/#11 VideoPlayer JS +html5/app11.js
+++ NEWprojectJS/#11 VideoPlayer JS +html5/app11.js
@@ -1,3 +1,17 @@
-<script>
   
-</script>
+
+  /* Get our elements */
+
+const player = document.querySelector(.'player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('.[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+
+    /* Build out functions */
+
+      /* Hook up the events listners*/
+

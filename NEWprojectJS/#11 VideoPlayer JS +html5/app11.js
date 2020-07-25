5998234343583ef46f7da5c2a968d2bffe17c6ca
Anna <bielechanna@gmail.com>
2020-07-25 14:43:29 +0200

    Added Nextchange#10

--- NEWprojectJS/#11 VideoPlayer JS +html5/app11.js
+++ NEWprojectJS/#11 VideoPlayer JS +html5/app11.js
@@ -17,5 +17,41 @@ function togglePlay() {
   video[method]();
 }
 
-      /* Hook up the events listners*/
+function updateButton() {
+   const icon = this.paused ? '►' : '❚ ❚';
+   console.log(icon);
+   toggle.textContent = icon; 
+}  
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  console.log(e);
+}
+
+/* Hook up the events listners*/
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+video.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', 'skip'));
+ranges.forEach(range => range.addEventListener('change', 'handleRangeUpdate'));
+ranges.forEach(range => range.addEventListener('mousemuve', 'handleRangeUpdate'));
+progress.addEventListener('click', scrub);
+
+
+
+
 

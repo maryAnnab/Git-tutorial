a9be0d0b293f203d2aad7c2714c1fa5d7799dfe2
Anna <bielechanna@gmail.com>
2020-08-14 17:40:16 +0200

    Added NextChange#19

--- NEWprojectJS/#19UnrealWebCam JS/scripts.js
+++ NEWprojectJS/#19UnrealWebCam JS/scripts.js
@@ -16,16 +16,78 @@ function getVideo() {
     });
 }
 
-function paintToCanvas() {
+function paintToCanavas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
   canvas.width = width;
   canvas.height = height;
 
-  setinterval(() => {
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    //take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    //mess with them
+   //pixels = redEffect(pixels);
+
+   pixels = rgbSplit(pixels);
+   ctx.globalAlpha = 0.8;
+   //put them back
+   ctx.putImageData(pixels, 0, 0);
 
   }, 16);
+}
+
+function takePhoto() {
+  //played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  //take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Womam" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i< pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // r
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;// g
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;// b
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i< pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // r
+    pixels.data[i + 500] = pixels.data[i + 1];// g
+    pixels.data[i - 550] = pixels.data[i + 2];// b
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+
+
+
+
+
+
+
 
 }
 
+
 getVideo();
+
+
+video.addEventListener('canplay', paintToCanavas);

26198705b0e12733cbe4575b410cd986fa1f86a6
Anna <bielechanna@gmail.com>
2020-08-14 10:48:54 +0200

    Added NewChange#19

--- NEWprojectJS/#19UnrealWebCam JS/scripts.js
+++ NEWprojectJS/#19UnrealWebCam JS/scripts.js
@@ -4,7 +4,28 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
-
 function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
 }
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  setinterval(() => {
+
+  }, 16);
+
+}
+
+getVideo();

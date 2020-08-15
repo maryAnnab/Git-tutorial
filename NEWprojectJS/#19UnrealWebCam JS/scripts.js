8ffc14f08207bfddc6a7fdc4c8f63296cc9a2ac9
Anna <bielechanna@gmail.com>
2020-08-15 11:42:05 +0200

    Added lastChange#19

--- NEWprojectJS/#19UnrealWebCam JS/scripts.js
+++ NEWprojectJS/#19UnrealWebCam JS/scripts.js
@@ -29,8 +29,9 @@ function paintToCanavas() {
     //mess with them
    //pixels = redEffect(pixels);
 
-   pixels = rgbSplit(pixels);
-   ctx.globalAlpha = 0.8;
+   //pixels = rgbSplit(pixels);
+   //ctx.globalAlpha = 0.8;
+   pixels = greenScreen(pixels);
    //put them back
    ctx.putImageData(pixels, 0, 0);
 
@@ -75,15 +76,26 @@ function greenScreen(pixels) {
   document.querySelectorAll('.rgb input').forEach((input) => {
     levels[input.name] = input.value;
   });
+ 
+   for (i=0; i < pixels.data.length; i= i+4){
 
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
 
-
-
-
-
-
-
-
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+          pixels.data[i + 3] = 0;
+        }
+    }
+ 
+    
+  return pixels;
 }
 
 
